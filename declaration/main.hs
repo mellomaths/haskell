a815d6f084af43f9d7d4f6b2bff4d1a27bbb921b
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-11-26 23:27:00 +0000

    Add declaration example

--- declaration/main.hs
+++ declaration/main.hs
@@ -6,4 +6,10 @@ v1 :: [Char] -- string as an array of characters
 -- v1 :: String
 v1 = foldr (++) "" (map show l1) 
 
+-- example using map
+-- map :: (a -> b) -> [a] -> [b]
+-- receives a function that transform the type 'a' to type 'b'
+-- and a list of elements of the type 'a'
+-- returns a list of elements of the type 'b'
+
 main = print v1
