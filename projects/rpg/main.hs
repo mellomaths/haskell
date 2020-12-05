cdf58dcd188da2c455686b93c3c36ff06579cd7d
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-05 20:30:04 -0300

    Simplify code using tuples and types

--- projects/rpg/main.hs
+++ projects/rpg/main.hs
@@ -1,76 +1,25 @@
-altura :: (String -> Int)
-altura classe = 
-    case classe of 
-        "Gigante" -> 20
-        "Guerreiro" -> 10
-        "Mago" -> 7
-        "Sacerdote" -> 8
-        "Gnomo" -> 3
+type Character = (Int, Int, Status)
+type Status = (Int, Int, Int)
 
-idade :: (String -> Int)
-idade classe = 
-    case classe of 
-        "Gigante" -> 156
-        "Guerreiro" -> 28
-        "Mago" -> 54
-        "Sacerdote" -> 87
-        "Gnomo" -> 92
+height :: Character -> Int
+height (h, _, _) = h
 
-forca :: (String -> Int)
-forca classe = 
-    case classe of 
-        "Gigante" -> 99
-        "Guerreiro" -> 78
-        "Mago" -> 24
-        "Sacerdote" -> 13
-        "Gnomo" -> 36
+age :: Character -> Int
+age (_, a, _) = a
 
-velocidade :: (String -> Int)
-velocidade classe = 
-    case classe of 
-        "Gigante" -> 14
-        "Guerreiro" -> 70
-        "Mago" -> 55
-        "Sacerdote" -> 25
-        "Gnomo" -> 92
+agility :: Character -> Int
+agility (_, _, (a, _, _)) = a
 
-magia :: (String -> Int)
-magia classe = 
-    case classe of 
-        "Gigante" -> 0
-        "Guerreiro" -> 0
-        "Mago" -> 75
-        "Sacerdote" -> 95
-        "Gnomo" -> 30
+power :: Character -> Int
+power (_, _, (_, p, _)) = p
 
-reza :: (String -> Int)
-reza classe =
-    case classe of
-        "Gigante" -> 0
-        "Guerreiro" -> 0
-        "Mago" -> 1
-        "Sacerdote" -> 1
-        "Gnomo" -> 0
+sourcery :: Character -> Int
+sourcery (_, _, (_, _, s)) = s
 
-luta :: (String -> Int)
-luta classe =
-    case classe of
-        "Gigante" -> 1
-        "Guerreiro" -> 1
-        "Mago" -> 1
-        "Sacerdote" -> 0
-        "Gnomo" -> 1
+giant = (8322, 156, (14, 99, 0))
+warrior = (187, 28, (70, 78, 0))
+mage = (156, 54, (44, 24, 78))
+priest = (210, 87, (25, 13, 95))
+gnome = (80, 92, (92, 40, 30))
 
-propriedades :: (String -> [Int])
-propriedades classe =
-    case classe of
-        "Gigante" -> [(altura "Gigante"), (idade "Gigante"), (forca "Gigante"), (velocidade "Gigante"), (magia "Gigante")]
-        "Guerreiro" -> [(altura "Guerreiro"), (idade "Guerreiro"), (forca "Guerreiro"), (velocidade "Guerreiro"), (magia "Guerreiro")]
-        "Mago" -> [(altura "Mago"), (idade "Mago"), (forca "Mago"), (velocidade "Mago"), (magia "Mago")]
-        "Sacerdote" -> [(altura "Sacerdote"), (idade "Sacerdote"), (forca "Sacerdote"), (velocidade "Sacerdote"), (magia "Sacerdote")]
-        "Gnomo" -> [(altura "Gnomo"), (idade "Gnomo"), (forca "Gnomo"), (velocidade "Gnomo"), (magia "Gnomo")]
-
-classes :: [[Int]]
-classes = [(propriedades "Gigante"), (propriedades "Guerreiro"), (propriedades "Mago"), (propriedades "Sacerdote"), (propriedades "Gnomo")]
-
-main = print (classes)
+main = print (age giant)
