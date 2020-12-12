fda450c7dcd7e18066befe9f7242d04d3767b5c7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-12 16:14:57 -0300

    Translate the definitions of the fold

--- tutorial/fold/main.hs
+++ tutorial/fold/main.hs
@@ -1,24 +1,38 @@
--- Função de alta ordem fold (reduce)
--- Fold é uma função de alta ordem pois recebe uma outra função como parâmetro
+-- Higher-Order Function "fold" (reduce)
+-- Fold is a higher-order function because it receives another function as an argument.
 
--- foldr itera da direita para esquerda
--- foldl itera da esquerda para direita
+-- foldr iterates from the right to the left of the list
+-- foldl iterates from the left to the right of the list
+
+-- so foldr works like this
+-- foldr f v0 l
+-- applies  f (ln, v0) -> v1
+--          f (ln-1, v1) -> v2
+--          f (ln-2, v2) -> v3
+--          ...
+--          f (l1, vn-1) -> vn (the final result)
 
 l1 = [10, 7, 4, 14, 1, 6]
 v1 = foldl (+) 0 l1
 -- v1 = 42 = 0+6+1+14+4+7+10
--- zero é o valor inicial da operação, no caso da soma é utilizado por ser neutro servindo como acumulador inicial
+-- zero is the initial value of the operation and "sum" (+) is being as inner function.
+-- zero is used because is the neutral value of sum.
 
-soma x y = x + y
-v2 = foldr soma 0 l1
+-- declaring another function instead of using (+).
+summ x y = x + y
+v2 = foldr summ 0 l1
 -- v2 = 42
 
+-- using an annoymous function.
 v3 = foldr (\x y -> x+y) 0 l1
 -- v2 = 42
 
 v4 = foldr (\x y -> (show x) ++ y) "" l1
 -- v4 = "10741416"
--- (show x) transforma o valor numérico em string
+-- (show x) transforms the numeric value of x to string.
+
+-- we can get the same result using the function "map" passing "show" as an argument.
+-- to transform each element of the list to string.
 v5 = foldr (++) "" (map show l1)
 -- v5 = "10741416"
 
@@ -27,6 +41,9 @@ v6 = foldr (-) 0 [1,2,3,4]
 
 v7 = foldl (-) 0 [1,2,3,4]
 -- -10
+
+-- because subtraction does not have comutative property as sums
+-- the results of foldr and foldl are differents.
 
 accounts = [1000, 6546.4, 542.5]
 sum_accounts acc1 acc2 = acc1 + acc2
