a4879570d3839c76826f62cef83fb513f4571101
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-12-11 23:49:15 -0300

    Add commentds

--- tutorial/filter/main.hs
+++ tutorial/filter/main.hs
@@ -1,5 +1,6 @@
--- Função de alta ordem filter
--- Filter é uma função de alta ordem pois recebe uma outra função como parâmetro
+-- Higher-Order Function -> filter
+-- Filter is a higher-order function because it receives another function as an argument.
+-- filter :: (a -> Bool) -> [a] -> [a]
 
 l1 = [10, 20, 30, 40]
 l2 = filter(>25) l1
@@ -15,22 +16,27 @@ l4 = filter(\x -> x > 25) l1
 
 l5 = filter(\x -> mod x 4 == 0) l1
 -- l5 = [20, 40]
--- pegando números divisiveis por 4
+-- getting numbers that are divisible by 4
 
 type Person = (String, Int, String)
-name (x,_,_) = x
-age (_,x,_) = x
-sex (_,_,x) = x
+name (x,_,_) = x -- returns the first element of the tuple
+age (_,x,_) = x -- returns the second element of the tuple
+sex (_,_,x) = x -- returns the third element of th tuple
 
+-- creates a list of the tuples (of the type of Person)
 people = [("Matheus", 23, "M"), ("Cristiana", 13, "F"), ("Messi", 27, "M")]
 
+-- function to check if the age of the person is greater than 18
 is_adult person = (age person) > 18
+-- filtering people's array returning only adult people
 adult_people = filter is_adult people
 
+-- function to check if the person has sex of "F" (female)
 is_female person = (sex person) == "F"
+-- function to check if the person has sex of "F" (male)
 is_male person = (sex person) == "M"
+-- filtering people's array returning only people with sex equals to "F"
 females = filter is_female people
-
 
 main = print females
 
